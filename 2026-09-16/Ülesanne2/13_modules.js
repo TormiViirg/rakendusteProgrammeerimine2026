d8967ad54699c2f3468b26f7aee6b852919402b4
Tormi Viirg <146323571+TormiViirg@users.noreply.github.com>
2026-09-16 16:54:19 +0300

    docs(modules): demonstrate distance conversion imports

---
+++ 2026-09-16/Ülesanne2/13_modules.js
@@ -0,0 +1,5 @@
+import { toKilometres, metresPerKilometre } from "./13_modules_helper.js";
+
+const trailMetres = 2750;
+console.log(`${metresPerKilometre} metres make one kilometre.`);
+console.log(`Trail distance: ${toKilometres(trailMetres)} km`); 
