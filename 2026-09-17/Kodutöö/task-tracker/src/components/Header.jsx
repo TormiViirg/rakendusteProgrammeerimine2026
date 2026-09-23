5e298eda76453c5e7d9b7e97b34b040fe81cbb57
Tormi Viirg <146323571+TormiViirg@users.noreply.github.com>
2026-09-23 16:52:45 +0300

    feat: task 3 JSX, Components and Basic Styling

---
+++ 2026-09-17/Kodutöö/task-tracker/src/components/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <header>My Tasks</header>;
+}
+
+export default Header;
---
+++ 2026-09-17/Kodutöö/task-tracker/src/components/TaskCard.jsx
@@ -0,0 +1,5 @@
+function TaskCard() {
+  return <div className="task-card">Conquer Baveria</div>;
+}
+
+export default TaskCard;
